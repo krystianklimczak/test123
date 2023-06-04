e88ea91bc6240e5c3f0e03bd506c2881adc5cec4
krystianklimczak <126380719+krystianklimczak@users.noreply.github.com>
2023-06-04 13:06:42 +0000

    Deploying to gh-pages from @ krystianklimczak/test123@553021c38cad0048339adaef7bd88df79e6f04b1 🚀

---
+++ index.b66c309e.js
@@ -0,0 +1,2 @@
+const swiper=new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.b66c309e.js.map
