424405c152ce7e3da3d31f3937c5a3a212e43036
krystianklimczak <krystianklimczak1996r@gmail.com>
2023-06-04 19:45:19 +0200

    add new animation for blog gallery

--- src/swiper.js
+++ src/swiper.js
@@ -1,6 +1,16 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
   loop: true,
 
   // If we need pagination
