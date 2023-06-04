180336eb09100a5fb3cbda7f3d9f41cc2d4c163f
krystianklimczak <126380719+krystianklimczak@users.noreply.github.com>
2023-06-04 17:46:07 +0000

    Deploying to gh-pages from @ krystianklimczak/test123@424405c152ce7e3da3d31f3937c5a3a212e43036 🚀

---
+++ index.0272d328.js
@@ -0,0 +1,2 @@
+const swiper=new Swiper(".swiper",{effect:"coverflow",grabCursor:!0,centeredSlides:!0,slidesPerView:"auto",coverflowEffect:{rotate:50,stretch:0,depth:100,modifier:1,slideShadows:!0},loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.0272d328.js.map
--- index.b66c309e.js
+++
@@ -1,2 +0,0 @@
-const swiper=new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.b66c309e.js.map
